b6603b4879fec3860f8d391fd9ff75dd99f4ad9c
bdvdcm <132024715+bdvdcm@users.noreply.github.com>
2023-05-26 20:52:47 -0500

    tercera fase

--- Ordenamiento1/Form1.cs
+++ Ordenamiento1/Form1.cs
@@ -12,7 +12,115 @@ namespace Ordenamiento1
 {
     public partial class Form1 : Form
     {
-       
+        bool estado = false;
+        int[] Arreglo_numeros;
+        Button[] Arreglo;
+
+        Numeros Datos = new Numeros();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void BubbleSort(ref int[] arreglo, ref Button[] Arreglo_Numeros)
+        {
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                for (int j = 0; j < arreglo.Length - 1; j++)
+                {
+                    if (arreglo[j] > arreglo[j + 1])
+                    {
+                        int aux = arreglo[j];
+                        arreglo[j] = arreglo[j + 1];
+                        arreglo[j + 1] = aux;
+                    }
+                }
+            }
+        }
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int num = Convert.ToInt32(txtNumero.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Solo se admiten numeros enteros");
+            }
+
+            estado = true;
+            tabPage1.Refresh();
+        }
+
+
+        private void tabPage1_Paint(object sender, PaintEventArgs e)
+        {
+            if (estado)
+            {
+                Point xy = new Point(50, 70);
+
+                try
+                {
+                    Dibujar_Arreglo(ref Arreglo, xy, ref tabPage1);
+                }
+
+                catch
+                {
+
+                }
+                estado = false;
+            }
+        }
+
+        public void Dibujar_Arreglo(ref Button[] Arreglo, Point xy, ref TabPage t)
+        {
+            for (int i = 1; i < Arreglo.Length; i++)
+            {
+                Arreglo[i].Location = xy;
+                t.Controls.Add(Arreglo[i]);
+                xy += new Size(70, 0);
+            }
+        }
+
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+
+            btnOrdenar.Enabled = false;
+            txtNumero.Enabled = false;
+            btnAgregar.Enabled = false;
+
+            BubbleSort(ref Arreglo_numeros, ref Arreglo);
+
+            this.Cursor = Cursors.Default;
+
+            btnOrdenar.Enabled = true;
+            txtNumero.Enabled = true;
+            btnAgregar.Enabled = true;
+
+
+        }
+
+
+        public void Intercambio(ref Button[] boton, int a, int b)
+        {
+
+        }
 
     }
 }
